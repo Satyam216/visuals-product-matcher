47c28bba7cc0f0381bd5cff761ebfe29d007fe60
Satyam216 <satyamjain216@gmail.com>
2025-10-15 00:12:54 +0530

    Update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000", // Backend URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
 });
 
 API.interceptors.request.use((config) => {
